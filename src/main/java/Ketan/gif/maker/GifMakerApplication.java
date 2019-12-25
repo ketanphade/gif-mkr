fb01181fc060b481eb97adf918c1eb30210b18bc
ketanphade <ketan.ak24@gmail.com>
2019-12-25 16:01:01 +0530

    added all new files

---
+++ src/main/java/Ketan/gif/maker/GifMakerApplication.java
@@ -0,0 +1,13 @@
+package Ketan.gif.maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifMakerApplication.class, args);
+	}
+
+}
